3e41bfb7e3d806012ccb7e85052f548e521395b2
Prabhat <prabhatdbg21@gmail.com>
2023-01-12 22:54:54 +0530

    users and cats images after feedback video

--- block-BJaajt/code/index.js
+++ block-BJaajt/code/index.js
@@ -1,3 +1,136 @@
+//Github User Finder
+let input = document.querySelector('input');
+let info = document.querySelector('.info');
+let userImage = document.querySelector('.info img');
+let userName = document.querySelector('.info h3');
+let userLogin = document.querySelector('.info p');
+let followersUL = document.querySelector('.followers');
+let followingUL = document.querySelector('.following');
+
+function fetch(url, successHandler){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url)
+    xhr.onload = () => successHandler(JSON.parse(xhr.response));
+    xhr.onerror = function () {
+        console.error(' Something went wrong ... ');
+    };
+    xhr.send();
+}
+
+/*
+function displayFollowers(username) {
+    followersUL.innerHTML = '' ; // to remove previous data
+    fetch(
+        `https://api.github.com/users/${username}/followers`, 
+        function(followersList){
+            let topFive = followersList.slice(0, 5);
+
+            topFive.forEach(info => {
+                let li = document.createElement('li');
+                let img = document.createElement('img');
+                img.src = info.avatar_url;
+                img.alt = info.name;
+                li.append(img);
+                followersUL.append(li);
+            })
+        }
+    );
+}
+
+function displayFollowing(username) {
+    followingUL.innerHTML = '' ; // to remove previous data
+    fetch(
+        `https://api.github.com/users/${username}/following`, 
+        function(followersList){
+            let topFive = followersList.slice(0, 5);
+
+            topFive.forEach(info => {
+                let li = document.createElement('li');
+                let img = document.createElement('img');
+                img.src = info.avatar_url;
+                img.alt = info.name;
+                li.append(img);
+                followingUL.append(li);
+            })
+        }
+    );
+}
+*/
+
+function displayExtraInfo(url, rootElm) {
+    rootElm.innerHTML = '' ; // to remove previous data
+    fetch(url, function(followersList){
+        let topFive = followersList.slice(0, 5);
+
+        topFive.forEach(info => {
+            let li = document.createElement('li');
+            let img = document.createElement('img');
+            img.src = info.avatar_url;
+            img.alt = info.name;
+            li.append(img);
+            rootElm.append(li);
+        })
+    });
+}
+
+function handleDisplay(userInfo) {
+    userImage.src = userInfo.avatar_url;
+    userImage.alt = userInfo.name;
+    userName.innerText = userInfo.name;
+    userLogin.innerText = '@' + userInfo.login;
+    /*
+    displayFollowers(userInfo.login);
+    displayFollowing(userInfo.login);
+    */
+    displayExtraInfo(`https://api.github.com/users/${userInfo.login}/followers`, followersUL);
+    displayExtraInfo(`https://api.github.com/users/${userInfo.login}/following`, followingUL);
+}
+
+function handleInput(event) {
+    if (event.keyCode === 13 && input.value){
+        const url = `https://api.github.com/users/`;
+        let username = input.value;
+        fetch(url + username, handleDisplay)
+
+        input.value = '';
+    }
+}
+
+input.addEventListener ('keydown', handleInput)
+
+
+
+
+//  Cats
+const catsImage = document.querySelector('.cats img');
+const catsButton = document.querySelector('.cats button');
+
+function handleClick(){
+    fetch ( `
+        https://api.thecatapi.com/v1/images/search?limit=1&size=full`, 
+        function(catInfo){
+            catsImage.src = catInfo[0].url;
+        }
+    )
+}
+
+catsButton.addEventListener('click', handleClick)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //cats
 
 const img = document.querySelector('.catImg');
@@ -21,7 +154,7 @@ reload.addEventListener('click', () => {
 
 // users
 
-const input = document.querySelector('input')
+const input = document.querySelector('input');
 const imgUser = document.querySelector('.userImg') ;
 const name = document.querySelector('h3');
 const followers = document.querySelector('.followers')
@@ -51,3 +184,4 @@ function handleChange(event) {
 }
 
 input.addEventListener ('keyup', handleChange)
+*/
